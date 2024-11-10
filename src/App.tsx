c6f8ff5cdc1b2c4d18dd46bfc2834d91554f8b74
Kamran Huseynov <huseynovk355@gmail.com>
2024-11-10 10:34:00 -0800

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+function App() {
+	return (
+		<Box>
+			<Heading
+				as="h1"
+				size="4xl"
+			>
+				Hello World!
+			</Heading>
+		</Box>
+	);
+}
+
+export default App;
