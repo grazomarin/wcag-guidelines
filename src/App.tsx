00de42b8d406f2bdf796ea643dcff75386dacf56
Justin Galimpin <jgalimpi@student.ubc.ca>
2024-11-10 19:21:06 -0800

    feat: added info text and popovers

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,65 @@
 import {
-	Accordion,
-	AccordionButton,
-	AccordionIcon,
-	AccordionItem,
-	AccordionPanel,
-	Box,
-	Heading,
-	useColorModeValue,
+    Accordion,
+    AccordionButton,
+    AccordionIcon,
+    AccordionItem,
+    AccordionPanel,
+    Box,
+    Heading,
+    useColorModeValue,
+    Text,
 } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 import Features from "./components/Features";
 
 export default function App() {
-	const textColor = useColorModeValue("gray.800", "white");
+    const textColor = useColorModeValue("gray.800", "white");
 
-	return (
-		<Box color={textColor}>
-			<Navbar />
-			<Box
-				display="grid"
-				gridAutoFlow="row"
-				gap="1rem"
-				p="1rem"
-			>
-				<Accordion
-					allowToggle
-					borderColor="transparent"
-				>
-					<AccordionItem>
-						<h2>
-							<AccordionButton
-								_hover={{
-									bg: "blue.100",
-								}}
-								borderRadius="1rem"
-							>
-								<Box
-									flex="1"
-									textAlign="left"
-								>
-									<Heading size="sm">About WCAG</Heading>
-								</Box>
-								<AccordionIcon />
-							</AccordionButton>
-						</h2>
-						<AccordionPanel>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet culpa deserunt ipsam
-							eaque fugiat eligendi, quidem quas ab sapiente doloribus, perspiciatis eveniet saepe
-							sequi reiciendis assumenda ipsum, hic sed! Asperiores.
-						</AccordionPanel>
-					</AccordionItem>
-				</Accordion>
+    return (
+        <Box color={textColor}>
+            <Navbar />
+            <Box
+                display="grid"
+                gridAutoFlow="row"
+                gap="1rem"
+                p="1rem"
+            >
+                <Accordion
+                    allowToggle
+                    borderColor="transparent"
+                >
+                    <AccordionItem>
+                        <h2>
+                            <AccordionButton
+                                _hover={{
+                                    bg: "blue.100",
+                                }}
+                                borderRadius="1rem"
+                            >
+                                <Box
+                                    flex="1"
+                                    textAlign="left"
+                                >
+                                    <Heading size="sm">About WCAG</Heading>
+                                </Box>
+                                <AccordionIcon />
+                            </AccordionButton>
+                        </h2>
+                        <AccordionPanel>
+                            This interactive web guide is designed for beginner web developers to help them understand the importance of following{" "}
+                            <Text as="span" fontWeight="bold">WCAG 2.0 (Web Content Accessibility Guidelines)</Text>. 
+                            Our goal is to make web development{" "}
+                            <Text as="span" fontWeight="bold">more inclusive </Text> 
+                            by teaching developers how to create accessible websites that everyone can use! 
+                            By using this tool, you’ll gain confidence in building websites that are{" "}
+                            <Text as="span" fontWeight="bold">functional, visually appealing, and accessible</Text> to all users, 
+                            regardless of their abilities.
+                        </AccordionPanel>
+                    </AccordionItem>
+                </Accordion>
 
-				<Features />
-			</Box>
-		</Box>
-	);
+                <Features />
+            </Box>
+        </Box>
+    );
 }
--- src/components/ColorGuidelines.tsx
+++ src/components/ColorGuidelines.tsx
@@ -1,74 +1,113 @@
-import { Box, Grid, Heading, Text } from "@chakra-ui/react";
+import { Box, Grid, Heading, Text, Popover, PopoverTrigger, PopoverArrow, PopoverBody, PopoverContent, Portal } from "@chakra-ui/react";
+import { QuestionIcon } from "@chakra-ui/icons";
 
 export default function ColorGuidelines({
-	contrast,
+    contrast,
 }: {
-	contrast: {
-		ratio: number;
-		passes: {
-			AA: {
-				normal: boolean;
-				large: boolean;
-			};
-			AAA: {
-				normal: boolean;
-				large: boolean;
-			};
-		};
-	};
+    contrast: {
+        ratio: number;
+        passes: {
+            AA: {
+                normal: boolean;
+                large: boolean;
+            };
+            AAA: {
+                normal: boolean;
+                large: boolean;
+            };
+        };
+    };
 }) {
-	return (
-		<Box
-			display="flex"
-			gap="1rem"
-			flexFlow="column"
-		>
-			<Heading size="md">WCAG 2.2 Guidelines</Heading>
-			<Grid
-				templateColumns="1fr 2fr 1fr"
-				templateRows="1fr 1fr 1fr 1fr"
-				gap=".5rem"
-			>
-				<Text as="strong">AA</Text>
-				<Text>Normal Text: </Text>
-				<Text
-					textAlign="center"
-					bgColor={contrast.passes.AA.normal ? "green.200" : "red.200"}
-					borderRadius="16px"
-				>
-					{contrast.passes.AA.normal ? "Pass" : "Fail"}
-				</Text>
+    return (
+        <Box 
+            display="flex" 
+            gap="1rem" 
+            flexFlow="column"
+        >
+            <Box display="flex" alignItems="center" gap="0.5rem">
+                <Heading size="md">WCAG 2.2 Guidelines</Heading>
 
-				<Text as="strong">AA</Text>
-				<Text>Large Text:</Text>
-				<Text
-					textAlign="center"
-					bgColor={contrast.passes.AA.large ? "green.200" : "red.200"}
-					borderRadius="16px"
-				>
-					{contrast.passes.AA.large ? "Pass" : "Fail"}
-				</Text>
+                <Popover>
+                    <PopoverTrigger>
+                        <QuestionIcon _hover={{ cursor: "pointer" }} />
+                    </PopoverTrigger>
+                    <Portal>
+                        <PopoverContent>
+                            <PopoverArrow />
+                            <PopoverBody>
+                                <Text>
+                                    <Text as="strong">AA</Text> is the WCAG conformance level which is considered the 
+                                    standard for most websites aiming for a medium level of accessibility. 
+                                    The contrast ratio between your text and background should be at least 
+                                    <Text as="strong"> 4.5:1</Text>. For large-scale text, it should be at least <Text as="strong">3:1</Text>.
+                                </Text>
+                            </PopoverBody>
+                        </PopoverContent>
+                    </Portal>
+                </Popover>
 
-				<Text as="strong">AAA</Text>
-				<Text>Normal Text: </Text>
-				<Text
-					textAlign="center"
-					bgColor={contrast.passes.AAA.normal ? "green.200" : "red.200"}
-					borderRadius="16px"
-				>
-					{contrast.passes.AAA.normal ? "Pass" : "Fail"}
-				</Text>
+                {/* Popover for AAA Guidelines */}
+                <Popover>
+                    <PopoverTrigger>
+                        <QuestionIcon _hover={{ cursor: "pointer" }} />
+                    </PopoverTrigger>
+                    <Portal>
+                        <PopoverContent>
+                            <PopoverArrow />
+                            <PopoverBody>
+                                <Text>
+                                    Although not required, <Text as="strong">AAA</Text> is the WCAG conformance 
+                                    level which represents the highest level of accessibility. The contrast ratio between text 
+                                    and background should be at least <Text as="strong">7:1</Text>. For large-scale text, 
+                                    it should be at least <Text as="strong">4.5:1</Text>.
+                                </Text>
+                            </PopoverBody>
+                        </PopoverContent>
+                    </Portal>
+                </Popover>
+            </Box>
 
-				<Text as="strong">AAA</Text>
-				<Text>Large Text:</Text>
-				<Text
-					textAlign="center"
-					bgColor={contrast.passes.AAA.large ? "green.200" : "red.200"}
-					borderRadius="16px"
-				>
-					{contrast.passes.AAA.large ? "Pass" : "Fail"}
-				</Text>
-			</Grid>
-		</Box>
-	);
+            <Grid templateColumns="1fr 2fr 1fr" templateRows="1fr 1fr 1fr 1fr" gap=".5rem">
+                <Text as="strong">AA</Text>
+                <Text>Normal Text: </Text>
+                <Text
+                    textAlign="center"
+                    bgColor={contrast.passes.AA.normal ? "green.200" : "red.200"}
+                    borderRadius="16px"
+                >
+                    {contrast.passes.AA.normal ? "Pass" : "Fail"}
+                </Text>
+
+                <Text as="strong">AA</Text>
+                <Text>Large Text:</Text>
+                <Text
+                    textAlign="center"
+                    bgColor={contrast.passes.AA.large ? "green.200" : "red.200"}
+                    borderRadius="16px"
+                >
+                    {contrast.passes.AA.large ? "Pass" : "Fail"}
+                </Text>
+
+                <Text as="strong">AAA</Text>
+                <Text>Normal Text: </Text>
+                <Text
+                    textAlign="center"
+                    bgColor={contrast.passes.AAA.normal ? "green.200" : "red.200"}
+                    borderRadius="16px"
+                >
+                    {contrast.passes.AAA.normal ? "Pass" : "Fail"}
+                </Text>
+
+                <Text as="strong">AAA</Text>
+                <Text>Large Text:</Text>
+                <Text
+                    textAlign="center"
+                    bgColor={contrast.passes.AAA.large ? "green.200" : "red.200"}
+                    borderRadius="16px"
+                >
+                    {contrast.passes.AAA.large ? "Pass" : "Fail"}
+                </Text>
+            </Grid>
+        </Box>
+    );
 }
